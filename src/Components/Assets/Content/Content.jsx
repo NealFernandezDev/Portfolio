4133ccac7b70aa046d8cf201f0a9839c3911c663
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-08-22 21:23:40 -0500

    excess br deleted in p

--- src/Components/Assets/Content/Content.jsx
+++ src/Components/Assets/Content/Content.jsx
@@ -7,18 +7,20 @@ const Content = () => {
       <div id='about'>
           <a href="#about"><h3>About</h3></a>
           <p>
-              My first experience with programming was my mid teens (34 now) when there<br/>
-              was a website that allowed us to create our own page that also had <br/>
-              a guest book where friends or anyone can write on. I helped <br/>
-              build pages for people as well as groups. But that ended once other different <br/>
-              types of social media outlets came out (myspace, facebook etc.)<br/>
+              My first experience with programming was my mid teens (34 now) when there
+              was a website that allowed us to create our own page that also had 
+              a guest book where friends or anyone can write on. I helped 
+              build pages for people as well as groups. But that ended once other different 
+              types of social media outlets came out (myspace, facebook etc.)
               <br/>
-              My main focus with my coding journey is to find a company I can learn and hopefully grow with.<br/>
-              What I enjoy most with coding currently is putting together a website a designer <br/>
-              has built (I am terrible at designing) and then making it a working reality for <br/>
-              others. To me it's like a puzzle.<br/>
               <br/>
-              When I am not working on a project, I'm usually playing video games, working out, <br/>
+              My main focus with my coding journey is to find a company I can learn and hopefully grow with.
+              What I enjoy most with coding currently is putting together a website a designer 
+              has built (I am terrible at designing) and then making it a working reality for 
+              others. To me it's like a puzzle.
+              <br/>
+              <br/>
+              When I am not working on a project, I'm usually playing video games, working out,
               watching movies, playings some sports, walking my dog or biking if the weather permits.
           </p> 
       </div>
