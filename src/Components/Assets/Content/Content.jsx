def38557c2d87e5825774b31c54259d5fb0454ef
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-08-26 10:35:03 -0500

    Content reworded in about

--- src/Components/Assets/Content/Content.jsx
+++ src/Components/Assets/Content/Content.jsx
@@ -22,17 +22,16 @@ const Content = () => {
       <div id='about'>
           <div id="line"></div><a href="#about"><h3>About</h3></a>
           <p>
+              My current focus with my coding journey is to find a company I can learn and hopefully grow with through 
+              an internship (associated with my school or not). What I enjoy most with coding is putting together a concept a designer 
+              has built and then making it a working reality for others.
+              <br/>
+              <br/>
               My first experience with programming was my mid teens (34 now) when there
               was a website that allowed us to create and design our own page/guest book where friends 
               or anyone can write on. A couple of friends and I 
               built pages for people as well as various groups. But that ended once other different 
               types of social media outlets came out (myspace, facebook etc.)
-              <br/>
-              <br/>
-              My current focus with my coding journey is to find a company I can learn and hopefully grow with through 
-              an internship. What I enjoy most with coding is putting together a concept a designer 
-              has built and then making it a working reality for 
-              others. To me coding is like a puzzle but instead functions, variables, components, and various others are the pieces.
               <br/>
               <br/>
               When I am not working on a project, I'm usually playing video games, working out,
